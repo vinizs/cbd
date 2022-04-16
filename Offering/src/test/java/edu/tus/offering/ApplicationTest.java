bc6d57a9816ef70e07811f9cef9ebb266c36171c
Vini Spader <vinizs@gmail.com>
2022-04-16 15:52:12 +0100

    house cleaning

--- Offering/src/test/java/edu/tus/offering/ApplicationTest.java
+++ Offering/src/test/java/edu/tus/offering/ApplicationTest.java
@@ -8,8 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Random;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
